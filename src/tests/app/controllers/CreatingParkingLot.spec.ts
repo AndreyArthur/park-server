aa0ce20c65dded09d18aeb65d34f0fbdb3d0c0eb
AndreyArthur <twekylaywer@gmail.com>
2021-05-03 14:30:39 -0300

    test: ensure that CreateParkingLotController returns correct status and body

---
+++ src/tests/app/controllers/CreatingParkingLot.spec.ts
@@ -0,0 +1,22 @@
+import { CreateParkingLotController } from '@/presentation/controllers';
+import { randomString } from '@/tests/helpers/generators';
+
+describe('CreateParkingLot Controller', () => {
+  it('should return status 201 and a ParkingLot in body', async () => {
+    const sut = new CreateParkingLotController();
+
+    const httpResponse = await sut.handle({
+      body: {
+        name: randomString(10),
+        password: randomString(8),
+      },
+    });
+
+    expect(httpResponse.status).toBe(201);
+    expect(httpResponse.body.password).toBe(undefined);
+    expect(httpResponse.body.id).toHaveLength(36);
+    expect(httpResponse.body.name).toHaveLength(10);
+    expect(typeof httpResponse.body.createdAt).toBe('object');
+    expect(typeof httpResponse.body.updatedAt).toBe('object');
+  });
+});
