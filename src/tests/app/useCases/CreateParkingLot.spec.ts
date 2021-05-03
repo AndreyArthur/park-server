90c6a198f864258b755d6759ef9edd08d820e451
AndreyArthur <twekylaywer@gmail.com>
2021-05-03 09:08:20 -0300

    test: ensure ParkingLot is created

---
+++ src/tests/app/useCases/CreateParkingLot.spec.ts
@@ -0,0 +1,52 @@
+import { randomString } from '@/tests/helpers';
+import { Encrypter } from '@/infra/adapters';
+import { CreateParkingLotUseCase } from '@/application/useCases';
+import { ParkingLotRepositoryMemory } from '@/infra/repositories';
+import { CreateParkingLot } from '@/core/useCases';
+import { AlreadyExistsError } from '@/core/exceptions';
+
+function makeSut(): CreateParkingLot {
+  const parkingLotRepository = new ParkingLotRepositoryMemory();
+  const sut = new CreateParkingLotUseCase(parkingLotRepository);
+
+  return sut;
+}
+
+describe('CreateParkingLot UseCase', () => {
+  it('should create a parking lot successfully', async () => {
+    const sut = makeSut();
+    const parkingLotPassword = randomString(8);
+    const parkingLot = await sut.execute({
+      name: randomString(12),
+      password: parkingLotPassword,
+    });
+
+    expect(parkingLot.id).toBeTruthy();
+    expect(parkingLot.name).toBeTruthy();
+    expect(await Encrypter.compare(parkingLotPassword, parkingLot.password))
+      .toBe(true);
+  });
+
+  it(
+    'should throw an AlreadyExistsError because Parking Lot already exists',
+    async () => {
+      const sut = makeSut();
+
+      const parkingLotName = randomString(8);
+
+      await sut.execute({
+        name: parkingLotName,
+        password: randomString(10),
+      });
+
+      try {
+        await sut.execute({
+          name: parkingLotName,
+          password: randomString(10),
+        });
+      } catch (err) {
+        expect(err).toEqual(new AlreadyExistsError('Parking Lot'));
+      }
+    },
+  );
+});
