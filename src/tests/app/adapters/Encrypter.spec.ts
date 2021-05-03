241669b417889bb106111455ff8d30545d6a9ed5
AndreyArthur <twekylaywer@gmail.com>
2021-05-03 09:36:11 -0300

    test: ensure that Encrypter return correct values in correct inputs

---
+++ src/tests/app/adapters/Encrypter.spec.ts
@@ -0,0 +1,31 @@
+import { Encrypter } from '@/infra/adapters';
+import { randomString } from '@/tests/helpers';
+
+describe('Encrypter Adapter', () => {
+  // To avoid jest coverage bugs with static methods
+  it('should create an instance of Encrypter', () => {
+    const encrypter = new Encrypter();
+
+    expect(encrypter).toBeInstanceOf(Encrypter);
+  });
+
+  it('should hash text successfully', async () => {
+    const textToBeHashed = randomString(8);
+    const hash = await Encrypter.encrypt(textToBeHashed);
+
+    expect(hash.length).toBe(60);
+    expect(hash !== textToBeHashed).toBe(true);
+  });
+
+  it('should compare a hash correctly', async () => {
+    const textToBeHashed = randomString(8);
+
+    const hashToBeCompared = await Encrypter.encrypt(textToBeHashed);
+
+    const secondHash = await Encrypter.encrypt(randomString(8));
+
+    expect(await Encrypter.compare(textToBeHashed, hashToBeCompared))
+      .toBe(true);
+    expect(await Encrypter.compare(textToBeHashed, secondHash)).toBe(false);
+  });
+});
