186d4f17cf4eec2e7afb34ec3c833334437f96f0
AndreyArthur <twekylaywer@gmail.com>
2021-05-09 11:50:16 -0300

    test: create a repository to follow DIP

---
+++ src/tests/app/repositories/CarMemory.spec.ts
@@ -0,0 +1,58 @@
+import { Car } from '@/core/entities';
+import { CarRepositoryMemory } from '@/infra/repositories';
+import { randomCarPlate } from '@/tests/helpers/generators';
+
+function makeSut(): CarRepositoryMemory {
+  return new CarRepositoryMemory();
+}
+
+describe('CarMemory Repository', () => {
+  it('should create a Car', () => {
+    const sut = makeSut();
+    const car = sut.create(randomCarPlate());
+
+    expect(car.id).toHaveLength(36);
+    expect(car.plate.match(/[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}/)).toBeTruthy();
+    expect(typeof car.createdAt).toBe('object');
+    expect(typeof car.updatedAt).toBe('object');
+  });
+
+  it('should find a Car', async () => {
+    const sut = makeSut();
+    const carPlate = randomCarPlate();
+
+    await sut.save(sut.create(carPlate));
+
+    const car = await sut.findByPlate(carPlate) as Car;
+
+    expect(car.id).toHaveLength(36);
+    expect(car.plate.match(/[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}/)).toBeTruthy();
+    expect(typeof car.createdAt).toBe('object');
+    expect(typeof car.updatedAt).toBe('object');
+  });
+
+  it('should save a 3 Cars', async () => {
+    const sut = makeSut();
+
+    async function saveCar(): Promise<Car> {
+      const car = sut.create(randomCarPlate());
+
+      await sut.save(car);
+
+      return car;
+    }
+
+    const savedCars: Car[] = [];
+
+    savedCars.push(await saveCar());
+    savedCars.push(await saveCar());
+    savedCars.push(await saveCar());
+
+    expect(savedCars[2].id).toHaveLength(36);
+    expect(
+      savedCars[2].plate.match(/[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}/),
+    ).toBeTruthy();
+    expect(typeof savedCars[2].createdAt).toBe('object');
+    expect(typeof savedCars[2].updatedAt).toBe('object');
+  });
+});
--- src/tests/app/useCases/CreateCar.spec.ts
+++ src/tests/app/useCases/CreateCar.spec.ts
@@ -1,14 +1,29 @@
+import { CarAlreadyRegisteredError } from '@/application/exceptions';
 import { CreateCarUseCase } from '@/application/useCases';
+import { CarRepositoryMemory } from '@/infra/repositories';
 import { randomCarPlate } from '@/tests/helpers/generators';
 
 describe('CreateCar useCase', () => {
   it('should create a Car successfully', async () => {
-    const sut = new CreateCarUseCase();
+    const sut = new CreateCarUseCase(new CarRepositoryMemory());
     const car = await sut.execute(randomCarPlate());
 
     expect(car.id).toHaveLength(36);
     expect(car.plate.match(/[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}/)).toBeTruthy();
     expect(typeof car.createdAt).toBe('object');
     expect(typeof car.updatedAt).toBe('object');
+  });
+
+  it('should throw an error because Car is already registered', async () => {
+    const sut = new CreateCarUseCase(new CarRepositoryMemory());
+    const carPlate = randomCarPlate();
+
+    await sut.execute(carPlate);
+
+    try {
+      await sut.execute(carPlate);
+    } catch (err) {
+      expect(err).toEqual(new CarAlreadyRegisteredError());
+    }
   });
 });
