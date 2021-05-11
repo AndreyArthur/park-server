7a041ee3d3f6e6fd81035f0afe749f2658fbaf69
AndreyArthur <twekylaywer@gmail.com>
2021-05-11 12:55:33 -0300

    test: cover Token error cases

--- src/tests/app/adapters/Token.spec.ts
+++ src/tests/app/adapters/Token.spec.ts
@@ -1,4 +1,8 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
 import { Token } from '@/application/adapters';
+import { AuthenticationError } from '@/application/exceptions';
 import { randomString } from '@/tests/helpers/generators';
 
 describe('Token Adapter', () => {
@@ -20,5 +24,28 @@ describe('Token Adapter', () => {
     const decoded = Token.decode(token);
 
     expect(decoded.content).toBe(tokenContent);
+  });
+
+  it('should throw an error because token is invalid', () => {
+    const token = Token.sign({ content: randomString(8) });
+
+    try {
+      Token.decode(`${randomString(4)}${token}${randomString(4)}`);
+    } catch (err) {
+      expect(err).toEqual(new AuthenticationError('invalid token'));
+    }
+  });
+
+  it('should throw an error because token is not a valid object', () => {
+    const token = jwt.sign({}, process.env.TOKEN_SECRET as string, {
+      expiresIn: 60,
+      subject: randomString(8),
+    });
+
+    try {
+      Token.decode(token);
+    } catch (err) {
+      expect(err).toEqual(new AuthenticationError('token is not parsable'));
+    }
   });
 });
